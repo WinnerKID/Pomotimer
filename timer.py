944e1866934e6c6ddb1a433e84b813a938be7348
WinnerKID <richy.ronkerboy@gmail.com>
2023-08-07 13:19:54 -0400

    changed default window size for a much smaller cleaner look

--- timer.py
+++ timer.py
@@ -47,7 +47,7 @@ layout = [
 ]
 
 # Create the window and set resizable attribute to True
-window = sg.Window("PomoTimer", layout, resizable=True)
+window = sg.Window("PomoTimer", layout, size=(250, 200), resizable=True)
 
 while True:
     event, values = window.read()
